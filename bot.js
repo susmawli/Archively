22d4d1528aaa73930b680541b6cd29dc55b7a5cd
SusMawLi <sushiboy225@gmail.com>
2022-05-19 22:42:39 -0700

    update 1.0.0.1

--- bot.js
+++ bot.js
@@ -1,18 +1,24 @@
 const Discord = require("discord.js")
 require("dotenv").config()
-const TOKEN = "OTc2MzcxNzYyNTM3MzEyMjc2.GNlSGe.3NSaUqpFY9uKqJsqvWd9lqCX2md1NORN6YMqXw"
 const client = new Discord.Client({
     intents: [
         "GUILDS",
         "GUILD_MESSAGES"
     ]
 })
-client.on("ready", () => {
-    console.log(`yoly`)
-})
-client.on("messageCreate", (message) => {
+let bot = {
+    client,
+    prefix: "process.env.",
+    owners: ["250195035792998400"]
+}
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadEvents(bot, false)
+module.exports = bot
+/*client.on("messageCreate", (message) => {
     if (message.content == "hi"){
         message.reply("hello world!")
     }
-})
+})*/
 client.login(process.env.TOKEN)
